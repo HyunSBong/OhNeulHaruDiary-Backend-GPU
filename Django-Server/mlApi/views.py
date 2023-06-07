0180561287263c669e5eac2d42a719671b54d17f
HyunSBong <69189272+HyunSBong@users.noreply.github.com>
2023-06-07 23:35:47 +0900

    url 오류 수정

--- Django-Server/mlApi/views.py
+++ Django-Server/mlApi/views.py
@@ -18,7 +18,10 @@ from . import diffusion
 
 import multiprocessing as mp
 
-kafka_topic = ('inference_prompt', 'image')
+## env
+from django.conf import settings
+
+kafka_topic = ('inference_diary', 'inference_dialogue', 'diffusion')
 
 class TestPostMessageKafkaView(CreateAPIView):
     model = MLItem
@@ -63,9 +66,12 @@ class SummaryDiaryView(CreateAPIView):
         prompt = self.request.data.get("prompt", None)
         url = self.request.data.get("url", None)
         thumbnail_url = self.request.data.get("thumbnail_url", None)
-        diary_id = self.request.data.get("diary_id", None)
+        diary_id = self.request.data.get('diary_id', None)
 
-        optimized_prompt = kn.to_kafka_summm_infer_diary(kafka_topic[0], diary_id, full_diary)
+        summarize = summ.inference_diary(full_diary) # input: String type
+        # print(summarize)
+        # summarize = "친구와 역에서 만났다."
+        optimized_prompt = promptist.promptist_manual(summarize)
 
         if serializer.is_valid():
             serializer.save(
@@ -83,14 +89,17 @@ class SummaryDialogueView(CreateAPIView):
     serializer_class = MLItemSerializer
 
     def perform_create(self, serializer):
-        req_urls = self.request.data.get('req_urls') # 얘는 꼭 필요 -> 이미지에서 가져와야함.
-        diary_id = self.request.data.get("diary_id", None)
+        req_urls = self.request.data.get('req_urls', None) # 얘는 꼭 필요 -> 이미지에서 가져와야함.
+        diary_id = self.request.data.get('diary_id', None)
         
+        AWS_STORAGE_BUCKET_NAME = getattr(settings, 'AWS_STORAGE_BUCKET_NAME', 'AWS_STORAGE_BUCKET_NAME')
+
         processes = []  
         manager = mp.Manager()
         return_dict = manager.dict()
         
         for idx, url in enumerate(req_urls):
+            url = "https://" + AWS_STORAGE_BUCKET_NAME + ".s3.ap-northeast-2.amazonaws.com/" + url[4:]
             process = mp.Process(target=recog.clova_ocr, args=(idx, url, return_dict,))
             processes.append(process)
             process.start()
@@ -101,11 +110,14 @@ class SummaryDialogueView(CreateAPIView):
         sorted_return_dict = sorted(return_dict.items())
         get_values = lambda lst: [value for _, value in lst]
         sum_dialogue = get_values(sorted_return_dict)
-        if len(req_urls) > 1:
-            get_values = lambda lst: [value[0] for value in lst]
-            sum_dialogue = get_values(sum_dialogue)
 
-        optimized_prompt = kn.to_kafka_summm_infer_diary(kafka_topic[0], diary_id, sum_dialogue)
+        get_values = lambda lst: [value[0] for value in lst]
+        sum_dialogue = get_values(sum_dialogue)
+        print(sum_dialogue)
+        
+        summarize = summ.inference_dialogue(sum_dialogue) # input: List type
+        print(summarize)
+        optimized_prompt = promptist.promptist_manual(summarize)
 
         if serializer.is_valid():
             serializer.save(
@@ -149,3 +161,4 @@ class GenerateImageCreateView(CreateAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED) 
         except:
             return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
