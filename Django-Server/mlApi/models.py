f834131dd2c4e6efcad45e42978ab3c8071faa5b
HyunSBong <69189272+HyunSBong@users.noreply.github.com>
2023-06-09 12:22:39 +0900

    글자수 제한 삭제

--- Django-Server/mlApi/models.py
+++ Django-Server/mlApi/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class MLItem(models.Model):
-    full_diary = models.TextField(max_length=500, null=True)
-    full_dialog = models.TextField(max_length=500, null=True)
-    prompt = models.TextField(max_length=200, null=True)
+    full_diary = models.TextField(null=True)
+    full_dialog = models.TextField(null=True)
+    prompt = models.TextField(null=True)
     url = models.TextField(null=True)
     thumbnail_url = models.TextField(null=True)
